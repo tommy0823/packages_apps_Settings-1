d442aaf5b0a9b230db75ea260d9dd58d0a71bbef
0xD34D <clark@scheffsblend.com>
2013-11-13 18:25:27 -0800

    Add native java screen recorder [2/2]
    PS2:
    Change framerate related stuff to bitrate since the setting
    is actually the bitrate and not the frame rate ;)
    
    Change-Id: I416a3c80eb4ba892497ce46afea979290d0460af
    
    Conflicts:
    	res/values/vendor_arrays.xml-->ck_arrays.xml
    	res/values/vendor_strings.xml-->ck_strings.xml
    	res/xml/display_settings.xml
    	src/com/android/settings/SecuritySettings.java
    
     Author:    0xD34D <clark@scheffsblend.com>

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -260,6 +260,30 @@ public class SecuritySettings extends RestrictedSettingsFragment
             }
         }
 
+        // Enable or disable keyguard widget checkbox based on DPM state
+        mEnableKeyguardWidgets = (CheckBoxPreference) root.findPreference(KEY_ENABLE_WIDGETS);
+        if (mEnableKeyguardWidgets != null) {
+            if (ActivityManager.isLowRamDeviceStatic()) {
+                // Widgets take a lot of RAM, so disable them on low-memory devices
+                PreferenceGroup securityCategory
+                        = (PreferenceGroup) root.findPreference(KEY_SECURITY_CATEGORY);
+                if (securityCategory != null) {
+                    securityCategory.removePreference(root.findPreference(KEY_ENABLE_WIDGETS));
+                    mEnableKeyguardWidgets = null;
+                }
+            } else {
+                final boolean disabled = (0 != (mDPM.getKeyguardDisabledFeatures(null)
+                        & DevicePolicyManager.KEYGUARD_DISABLE_WIDGETS_ALL));
+                if (disabled) {
+                    mEnableKeyguardWidgets.setSummary(
+                            R.string.security_enable_widgets_disabled_summary);
+                } else {
+                    mEnableKeyguardWidgets.setSummary("");
+                }
+                mEnableKeyguardWidgets.setEnabled(!disabled);
+            }
+        }
+
         // lock after preference
         mLockAfter = (ListPreference) root.findPreference(KEY_LOCK_AFTER_TIMEOUT);
         if (mLockAfter != null) {
