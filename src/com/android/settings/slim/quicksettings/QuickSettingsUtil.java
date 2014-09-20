fc219ab249015a0ac024aecd51fe43f75bcff79e
rc420head <dennislebhardt@yahoo.com>
2014-09-20 07:51:44 -0500

    Revert "Add On-The-Go Toggle [2/2]"
    This reverts commit 748ea3d93a8637669e99a8471b10e5483d5e9131.

--- src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
@@ -15,7 +15,6 @@
  * limitations under the License.
  */
 
-
 package com.android.settings.slim.quicksettings;
 
 import static com.android.internal.util.slim.QSConstants.TILES_DEFAULT;
@@ -58,7 +57,6 @@ import static com.android.internal.util.slim.QSConstants.TILE_WIFI;
 import static com.android.internal.util.slim.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.slim.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.slim.QSConstants.TILE_REMOTEDISPLAY;
-import static com.android.internal.util.slim.QSConstants.TILE_ONTHEGO;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -177,9 +175,6 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_SHAKE, R.string.title_tile_shake,
                 "com.android.systemui:drawable/ic_qs_shake_events"));
-        registerTile(new QuickSettingsUtil.TileInfo(
-                TILE_ONTHEGO, R.string.title_tile_onthego,
-                "com.android.systemui:drawable/ic_qs_onthego"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
