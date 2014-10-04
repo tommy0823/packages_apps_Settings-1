7f2d04fef62847ed0782dab72e5f1df04b81fd4d
rc420head <dennislebhardt@yahoo.com>
2014-10-04 15:01:14 -0500

    Settings: Heads Up background color && transparency [1/2] PS12: squash dependency - heads up settings now under interface menu
    PS: add ability to set custom heads up text color
    
    Change-Id: I606bf35d5db9a253448971bdedf6fdb3acad6452
     dankoman30 authored 18 days ago

--- src/com/android/settings/candykat/HeadsUp.java
+++ src/com/android/settings/candykat/HeadsUp.java
@@ -1,55 +1,86 @@
- package com.android.settings.candykat;
-  
-  import android.content.pm.PackageManager;
-  import android.content.res.Resources;
-  import android.media.Ringtone;
-  import android.media.RingtoneManager;
-  import android.net.Uri;
-  import android.os.Bundle;
-  import android.preference.CheckBoxPreference;
-  import android.preference.ListPreference;
-  import android.preference.Preference;
-  import android.preference.Preference.OnPreferenceChangeListener;
-  import android.preference.PreferenceCategory;
-  import android.preference.PreferenceScreen;
-  import android.preference.RingtonePreference;
-  import android.preference.SlimSeekBarPreference;
-  import android.provider.Settings;
-  import android.os.UserHandle;
-  
-  import com.android.internal.util.slim.DeviceUtils;
-  
-  import com.android.settings.SettingsPreferenceFragment;
-  import com.android.settings.slim.quicksettings.QuickSettingsUtil;
-  import com.android.settings.R;
+/*
+ * Copyright (C) 2012 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-public class HeadsUp extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+package com.android.settings.candykat;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.os.UserHandle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class HeadsUp extends SettingsPreferenceFragment
+            implements OnPreferenceChangeListener  {
+
+    public static final String TAG = "HeadsUpSettings";
 
     // Default timeout for heads up snooze. 5 minutes.
     protected static final int DEFAULT_TIME_HEADS_UP_SNOOZE = 300000;
 
-    private static final String PREF_HEADS_UP_EXPANDED = "heads_up_expanded";
-    private static final String PREF_HEADS_UP_SNOOZE_TIME = "heads_up_snooze_time";
-    private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
-    private static final String PREF_HEADS_UP_SHOW_UPDATE = "heads_up_show_update";
-    private static final String PREF_HEADS_UP_GRAVITY = "heads_up_gravity";
-    private static final String PREF_HEADS_UP_EXCLUDE_FROM_LOCK_SCREEN = "heads_up_exclude_from_lock_screen";
+    private static final String PREF_HEADS_UP_EXPANDED =
+            "heads_up_expanded";
+    private static final String PREF_HEADS_UP_SNOOZE_TIME =
+            "heads_up_snooze_time";
+    private static final String PREF_HEADS_UP_TIME_OUT =
+            "heads_up_time_out";
+    private static final String PREF_HEADS_UP_SHOW_UPDATE =
+            "heads_up_show_update";
+    private static final String PREF_HEADS_UP_GRAVITY =
+            "heads_up_gravity";
+    private static final String HEADS_UP_BG_COLOR =
+            "heads_up_bg_color";
+    private static final String HEADS_UP_TEXT_COLOR =
+            "heads_up_text_color";
 
     ListPreference mHeadsUpSnoozeTime;
     ListPreference mHeadsUpTimeOut;
     CheckBoxPreference mHeadsUpExpanded;
     CheckBoxPreference mHeadsUpShowUpdates;
     CheckBoxPreference mHeadsUpGravity;
-    CheckBoxPreference mHeadsExcludeFromLockscreen;
+
+    private ColorPickerPreference mHeadsUpBgColor;
+    private ColorPickerPreference mHeadsUpTextColor;
+
+    private static final int MENU_RESET = Menu.FIRST;
+    private static final int DEFAULT_BACKGROUND_COLOR = 0x00ffffff;
+    private static final int DEFAULT_TEXT_COLOR = 0xffffffff;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.heads_up_settings);
 
-        addPreferencesFromResource(R.xml.headsup_settings);
-
-        PreferenceScreen prefSet = getPreferenceScreen();
+        PreferenceScreen prefs = getPreferenceScreen();
 
         PackageManager pm = getPackageManager();
 
@@ -67,11 +98,6 @@ public class HeadsUp extends SettingsPreferenceFragment implements
         mHeadsUpGravity.setChecked(Settings.System.getIntForUser(getContentResolver(),
                 Settings.System.HEADS_UP_GRAVITY_BOTTOM, 0, UserHandle.USER_CURRENT) == 1);
         mHeadsUpGravity.setOnPreferenceChangeListener(this);
-
-        mHeadsExcludeFromLockscreen = (CheckBoxPreference) findPreference(PREF_HEADS_UP_EXCLUDE_FROM_LOCK_SCREEN);
-        mHeadsExcludeFromLockscreen.setChecked(Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.HEADS_UP_EXCLUDE_FROM_LOCK_SCREEN, 0, UserHandle.USER_CURRENT) == 1);
-        mHeadsExcludeFromLockscreen.setOnPreferenceChangeListener(this);
 
         mHeadsUpSnoozeTime = (ListPreference) findPreference(PREF_HEADS_UP_SNOOZE_TIME);
         mHeadsUpSnoozeTime.setOnPreferenceChangeListener(this);
@@ -95,16 +121,36 @@ public class HeadsUp extends SettingsPreferenceFragment implements
                 Settings.System.HEADS_UP_NOTIFCATION_DECAY, defaultTimeOut);
         mHeadsUpTimeOut.setValue(String.valueOf(headsUpTimeOut));
         updateHeadsUpTimeOutSummary(headsUpTimeOut);
-    }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
+        // Heads Up background color
+        mHeadsUpBgColor =
+                (ColorPickerPreference) findPreference(HEADS_UP_BG_COLOR);
+        mHeadsUpBgColor.setOnPreferenceChangeListener(this);
+        final int intColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_BG_COLOR, 0x00ffffff);
+        String hexColor = String.format("#%08x", (0x00ffffff & intColor));
+        if (hexColor.equals("#00ffffff")) {
+            mHeadsUpBgColor.setSummary(R.string.trds_default_color);
+        } else {
+            mHeadsUpBgColor.setSummary(hexColor);
+        }
+        mHeadsUpBgColor.setNewPreviewColor(intColor);
 
-    @Override
-    public void onPause() {
-        super.onPause();
+        // Heads Up text color
+        mHeadsUpTextColor =
+                (ColorPickerPreference) findPreference(HEADS_UP_TEXT_COLOR);
+        mHeadsUpTextColor.setOnPreferenceChangeListener(this);
+        final int intTextColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_TEXT_COLOR, 0x00000000);
+        String hexTextColor = String.format("#%08x", (0x00000000 & intTextColor));
+        if (hexTextColor.equals("#00000000")) {
+            mHeadsUpTextColor.setSummary(R.string.trds_default_color);
+        } else {
+            mHeadsUpTextColor.setSummary(hexTextColor);
+        }
+        mHeadsUpTextColor.setNewPreviewColor(intTextColor);
+        setHasOptionsMenu(true);
+
     }
 
     @Override
@@ -138,14 +184,35 @@ public class HeadsUp extends SettingsPreferenceFragment implements
                     Settings.System.HEADS_UP_GRAVITY_BOTTOM,
                     (Boolean) newValue ? 1 : 0, UserHandle.USER_CURRENT);
             return true;
-        } else if (preference == mHeadsExcludeFromLockscreen) {
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.HEADS_UP_EXCLUDE_FROM_LOCK_SCREEN,
-                    (Boolean) newValue ? 1 : 0, UserHandle.USER_CURRENT);
+        } else if (preference == mHeadsUpBgColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            if (hex.equals("#00ffffff")) {
+                preference.setSummary(R.string.trds_default_color);
+            } else {
+                preference.setSummary(hex);
+            }
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_BG_COLOR,
+                    intHex);
             return true;
-         }
-          return false;
-      }
+        } else if (preference == mHeadsUpTextColor) {
+            String hexText = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            if (hexText.equals("#00000000")) {
+                preference.setSummary(R.string.trds_default_color);
+            } else {
+                preference.setSummary(hexText);
+            }
+            int intHexText = ColorPickerPreference.convertToColorInt(hexText);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_TEXT_COLOR,
+                    intHexText);
+            return true;
+        }
+        return false;
+    }
 
     private void updateHeadsUpSnoozeTimeSummary(int value) {
         String summary = value != 0
@@ -166,7 +233,44 @@ public class HeadsUp extends SettingsPreferenceFragment implements
     }
 
     @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_RESET, 0, R.string.reset_default_message)
+                .setIcon(R.drawable.ic_settings_backup)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                resetToDefault();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    private void resetToDefault() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+        alertDialog.setTitle(R.string.shortcut_action_reset);
+        alertDialog.setMessage(R.string.qs_style_reset_message);
+        alertDialog.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                resetValues();
+            }
+        });
+        alertDialog.setNegativeButton(R.string.cancel, null);
+        alertDialog.create().show();
+    }
+
+    private void resetValues() {
+        Settings.System.putInt(getContentResolver(),
+                Settings.System.HEADS_UP_BG_COLOR, DEFAULT_BACKGROUND_COLOR);
+        mHeadsUpBgColor.setNewPreviewColor(DEFAULT_BACKGROUND_COLOR);
+        mHeadsUpBgColor.setSummary(R.string.trds_default_color);
+        Settings.System.putInt(getContentResolver(),
+                Settings.System.HEADS_UP_TEXT_COLOR, 0);
+        mHeadsUpTextColor.setNewPreviewColor(DEFAULT_TEXT_COLOR);
+        mHeadsUpTextColor.setSummary(R.string.trds_default_color);
     }
 }
