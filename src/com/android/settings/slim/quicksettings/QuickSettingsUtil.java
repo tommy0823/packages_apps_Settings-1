e42404d8d62b71c39f9c0e98e8b65d18f87f8a7a
rc420head <dennislebhardt@yahoo.com>
2014-10-12 13:39:40 -0500

    Revert "Add On-The-Go Toggle [2/2]"
    This reverts commit 233672ec2a6f15e2de12a4f0eed73d77bd94427e.

--- src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
@@ -58,7 +58,6 @@ import static com.android.internal.util.slim.QSConstants.TILE_WIFI;
 import static com.android.internal.util.slim.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.slim.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.slim.QSConstants.TILE_REMOTEDISPLAY;
-import static com.android.internal.util.slim.QSConstants.TILE_ONTHEGO;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -180,9 +179,6 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_NETWORKADB, R.string.title_tile_network_adb,
                 "com.android.systemui:drawable/ic_qs_network_adb_off"));
-        registerTile(new QuickSettingsUtil.TileInfo(
-                TILE_ONTHEGO, R.string.title_tile_onthego,
-                "com.android.systemui:drawable/ic_qs_onthego"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
